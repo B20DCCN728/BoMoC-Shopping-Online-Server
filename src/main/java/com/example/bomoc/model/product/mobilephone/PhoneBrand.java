912b29f22709d29d429de80fcad54ab3010e5854
B20DCCN728 <vietjohnny810@gmail.com>
2023-12-22 01:03:44 +0700

    Update today :(

--- src/main/java/com/example/bomoc/model/product/mobilephone/PhoneBrand.java
+++ src/main/java/com/example/bomoc/model/product/mobilephone/PhoneBrand.java
@@ -1,8 +1,7 @@
 package com.example.bomoc.model.product.mobilephone;
 
 import com.example.bomoc.model.product.mobilephone.MobilePhone;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,11 +16,20 @@ import java.util.*;
 @AllArgsConstructor
 public class PhoneBrand {
 
+	@OneToMany(mappedBy = "phoneBrand", cascade = CascadeType.ALL)
 	private List<MobilePhone> mobilePhones;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID", nullable = false)
 	private int ID;
+	@Column(name = "BrandName", nullable = false)
 	private String brandName;
+	@Column(name = "Country", nullable = false)
 	private String country;
+	@Column(name = "YearEstablished", nullable = false)
 	private LocalDateTime yearEstablished;
+	@Column(name = "Attribute", nullable = false)
 	private int attribute;
 
 }
